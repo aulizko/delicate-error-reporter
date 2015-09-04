ace516fafd03ad8808a1f3ac18889d76ffb1fbaa
Alexander Ulizko <aulizko@gmail.com>
2015-09-05 01:36:04 +0300

    Switched from react-classy to j2c

--- src/index.js
+++ src/index.js
@@ -1,55 +1,47 @@
 import React, { Component, PropTypes } from 'react';
 import ErrorStackParser from 'error-stack-parser';
-import Classy from 'react-classy';
+import j2c from 'j2c';
 
-@Classy
+const styles = j2c.scoped({
+  container: {
+    'box-sizing': 'border-box',
+    'font-family': 'sans-serif',
+    'font-size': '1em',
+    'position': 'fixed',
+    'padding': '10px',
+    'top': '0',
+    'bottom': '0',
+    'left': '0',
+    'width': '100%',
+    'background': 'rgb(0 0 204)',
+    'color': 'white'
+  },
+  message: {
+    'font-weight': 'bold'
+  },
+  stack: {
+    'font-family': 'monospace',
+    'margin-top': '2em'
+  },
+  'frame': {
+    'margin-top': '1em'
+  },
+  'file': {
+    'font-size': '0.8em',
+    'color': 'rgba(255, 255, 255, 0.7)'
+  },
+  fileLink: {
+    'text-decoration': 'none',
+    'color': 'rgba(255, 255, 255, 0.7)'
+  }
+});
+
 class DelicateErrorReporter extends Component {
   static displayName = 'DelicateErrorReporter';
 
   static propTypes = {
     error: PropTypes.instanceOf(Error).isRequired
   };
-
-  static style = `
-    .redbox {
-      box-sizing: border-box;
-      font-family: sans-serif;
-      font-size: 1em;
-      position: fixed;
-      padding: 10px;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      background: rgb(0 0 204);
-      color: white;
-    }
-
-    .message {
-      font-weight: bold;
-    }
-
-    .stack {
-      font-family: monospace;
-      margin-top: 2em;
-    }
-
-    .frame {
-      margin-top: 1em;
-    }
-
-    .file {
-      font-size: 0.8em;
-      color: #ffffff;
-      color: rgba(255, 255, 255, 0.7);
-    }
-
-    .fileLink {
-      text-decoration: none;
-      color: #ffffff;
-      color: rgba(255, 255, 255, 0.7);
-    }
-  `;
 
   render() {
     const { error } = this.props;
@@ -60,17 +52,20 @@ class DelicateErrorReporter extends Component {
       return (
         <div className="frame" key={ key }>
           <div>{ f.functionName }</div>
-          <div className="file">
-            <a href={ link } className="fileLink">{ link }</a>
+          <div className={ styles.file }>
+            <a href={ link } className={ styles.fileLink }>{ link }</a>
           </div>
         </div>
       )
     });
 
     return (
-      <div className="redbox">
-        <div className="message">{ error.name }: { error.message }</div>
-        <div className="stack">{ frames }</div>
+      <div>
+        <style>{styles.valueOf()}</style>
+        <div className={ styles.container }>
+          <div className={ styles.message }>{ error.name }: { error.message }</div>
+          <div className={ styles.stack }>{ frames }</div>
+        </div>
       </div>
     )
   }
